d81af034d93e9ba6c168b88617d4c9053e10283b
Arne Schauf <arne.schauf@selfnet.de>
2013-03-19 00:30:55 +0100

    sender okay. use python sender.py

--- sender.py
+++ sender.py
@@ -10,31 +10,10 @@ COMMANDS = {
     'setSmoothfadeTime': struct.pack('B', 104),#struct(104+time) with 2bytes time in milliseconds
 }
 
-# set up logging to file - see previous section for more details
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-                    datefmt='%d.%m.%y %H:%M:%s',
-                    filename='sender.log',
-                    filemode='w')
-#logging.critical("Critical")
-#logging.error("Error")
-#logging.warning("Warning")
-#logging.info("Info")
-#logging.debug("Debug")
-
 class Sender():
     def __init__(self, host='10.43.100.111', port=23):
         self.host = host
         self.port = port
-        #signal.signal(signal.SIGTERM, self.signal_handler)
-        for i in [x for x in dir(signal) if x.startswith("SIG")]:
-            try:
-                signum = getattr(signal,i)
-                signal.signal(signum,self.signal_handler)
-            except RuntimeError,m:
-                print "Skipping %s"%i
-            except ValueError,m:
-                print "Skipping %s"%i
         self.connManager = ConnectionManager(host, port)
         self.connManager.start()
 
@@ -67,17 +46,23 @@ class Sender():
     def police(self):
         while True:
             for i in range(0,2):
+                print "red"
                 self.setColor(255,0,0)
                 time.sleep(0.1)
+                print "blue"
                 self.setColor(0,0,255)
                 time.sleep(0.1)
+            print "black"
             self.setColor(0,0,0)
             time.sleep(0.08)
             for i in range(0,2):
+                print "red"
                 self.setColor(255,0,0)
                 time.sleep(0.1)
+                print "blue"
                 self.setColor(0,0,255)
                 time.sleep(0.1)
+            print "white"
             self.setColor(255,255,255)
             time.sleep(0.08)
             
@@ -133,11 +118,8 @@ class Sender():
             time.sleep(sleep)
             
     def stop(self):
+        logging.debug('Received stop...')
         self.connManager.stop()
-
-    def signal_handler(signal, frame):
-            print 'signal %s received'%signal
-            self.stop()
 
 class ConnectionManager(threading.Thread):
     def __init__(self,  host, port):
@@ -161,20 +143,25 @@ class ConnectionManager(threading.Thread):
                 try:
                     self.lastReceived = 'untouched'
                     self.lastReceived = self.sock.recv(4096)
-                    if self.lastReceived:
-                        logging.debug("received: %s"%self.lastReceived)
-                        self.recvLock.release()
-                        self.recvLock.acquire()
-                    else:
-                        self.connectionReset()
+                    if not self.stopped:
+                        if self.lastReceived:
+                            logging.debug("received: %s"%self.lastReceived)
+                            self.recvLock.release()
+                            self.recvLock.acquire()
+                        else:
+                            self.connectionReset()
                 except socket.timeout, e:
-                    logging.debug(e)
+                    pass
                 except Exception, e:
-                    logging.debug(e)
+                    logging.debug('in receive exception: %s'%str(e))
                     self.connectionReset()
         logging.info('Connection closed, ConnectionManager stopped')
     
     def connectionReset(self):
+        logging.debug("connection reset")
+        print "Connection lost. Reconnecting..."
+        self.sendLock.acquire(False)
+        self.sendLock.release()
         self.sendLock.acquire()
         self.connected = False
         self.lastReceived = 'Connection error'
@@ -187,9 +174,13 @@ class ConnectionManager(threading.Thread):
         return self.lastReceived
     
     def connect(self):
+        self.sendLock.acquire(False)
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.settimeout(1)
         self.sock.connect((self.host, self.port))
         self.connected = True
+        logging.debug("connection established")
+        print "Connected to %s:%d"%(self.host,self.port)
         self.sendLock.release()
     
     def stop(self):
@@ -199,45 +190,107 @@ class ConnectionManager(threading.Thread):
     def disconnect(self):
         self.sendLock.acquire(False)
         self.sock.close()
+        logging.debug("disconnect")
         self.connected = False
     
     def tryconnect(self):
         self.sendLock.acquire(False)
-        self.sock.settimeout(1)
         try:
             self.disconnect()
         except:
             pass
         logging.debug("Trying to connect...")
-        while not self.connected:
+        while not self.connected and not self.stopped:
             try:
                 self.connect()
-                logging.info("Connected")
+                logging.info("Connected to %s:%d"%(self.host,self.port))
             except socket.timeout:
                 pass
             except Exception, e:
                 logging.info(e)
                 time.sleep(0.1)
                 logging.debug("Trying to connect...")
-        self.sock.settimeout(0)
-        self.sock.setblocking(True)
 
     def send(self, msg):
-        logging.info("waiting for sendLock...")
-        self.sendLock.acquire()
-        logging.info("sendLock acquired")
         if self.connected:
+            logging.info("waiting for sendLock...")
+            self.sendLock.acquire()
+            logging.info("sendLock acquired")
             logging.info("sending unicode %s"%msg)
             binmsg = ''
             for c in msg:
                 binmsg += Helper.format8bit(bin(struct.unpack('B',c)[0])) + ' '
             logging.info("sending binary %s"%binmsg)
-            self.sock.sendall(msg)
+            try:
+                self.sock.sendall(msg)
+            except Exception, e:
+                logging.warn('Exception when sending: %s'%str(e))
+                print "Connection error. Reconnecting..."
+                self.sendLock.release()
+                self.connectionReset()
+            self.sendLock.release()
         else:
             logging.warn("Not sent. Socket not connected")
-        self.sendLock.release()
+        
 
 class Helper:
     @staticmethod
     def format8bit(binstr):
         return ('0'*(8-len(binstr[2:]))) + binstr[2:]
+
+def main():
+    # set up logging to file - see previous section for more details
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                        datefmt='%d.%m.%y %H:%M:%s',
+                        filename='sender.log',
+                        filemode='a')
+    #logging.critical("Critical")
+    #logging.error("Error")
+    #logging.warning("Warning")
+    #logging.info("Info")
+    #logging.debug("Debug")
+    
+    global s
+    s = Sender()
+
+    #######################
+    #fancy startup shit
+    #######################
+    import atexit
+    import os
+    import readline
+    import rlcompleter
+    historyPath = os.path.expanduser("~/.pyhistory")
+    historyTmp = os.path.expanduser("~/.pyhisttmp.py")
+    endMarkerStr= "# # # histDUMP # # #"
+    saveMacro= "import readline; readline.write_history_file('"+historyTmp+"'); \
+        print '####>>>>>>>>>>'; print ''.join(filter(lambda lineP: \
+        not lineP.strip().endswith('"+endMarkerStr+"'),  \
+        open('"+historyTmp+"').readlines())[:])+'####<<<<<<<<<<'"+endMarkerStr
+    readline.parse_and_bind('tab: complete')
+    readline.parse_and_bind('\C-w: "'+saveMacro+'"')
+    def save_history(historyPath=historyPath, endMarkerStr=endMarkerStr):
+        import readline
+        readline.write_history_file(historyPath)
+        # Now filter out those line containing the saveMacro
+        lines= filter(lambda lineP, endMarkerStr=endMarkerStr:
+                          not lineP.strip().endswith(endMarkerStr), open(historyPath).readlines())
+        open(historyPath, 'w+').write(''.join(lines))
+    if os.path.exists(historyPath):
+        readline.read_history_file(historyPath)
+    atexit.register(save_history)
+
+    del os, atexit, readline, rlcompleter, save_history, historyPath
+    del historyTmp, endMarkerStr, saveMacro
+    #######################
+    #end fancy startup shit
+    #######################
+
+    import code
+    code.InteractiveConsole(locals=globals()).interact("s is your Sender! try 's.police()'")
+    print "Stopping network daemon..."
+    s.stop()
+    print "Stopped. Exiting..."
+if __name__ == "__main__":
+    main()
